175246646a156675dc64875933402a84f5db07e1
yamamotoreina <rainbowsunny953@gmail.com>
2025-03-25 18:10:43 +0900

    feat:index.tsx addition and App.tsx delete

---
+++ src/app/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+
+const Index = (): JSX.Element => {
+  return(
+    <View>
+      <Text>Hello World</Text>
+    </View>
+  )
+  
+}
+
+export default Index

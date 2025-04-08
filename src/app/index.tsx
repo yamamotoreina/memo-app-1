3a4d35e3469ff7884493c9d1cede86cc08152761
yamamotoreina <rainbowsunny953@gmail.com>
2025-04-08 18:23:53 +0900

    index.tsx update

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,55 +1,25 @@
 import { View, Text, StyleSheet } from 'react-native'
+
+import Header from '../components/Header'
+import MemoListItem from '../components/MemoListItem'
+import CircleButton from '../components/CircleButton'
+
 //memoItemの構造化
 const Index = (): JSX.Element => {
   return(
     <View style={styles.container}>
 
-      <View style={styles.header}>
-        <View style={styles.headerInner}>
-          <Text style={styles.headerTitle}>Recipe App</Text>
-          <Text style={styles.headerRight}>ログアウト</Text>
-        </View>
-      </View>
+      <Header />
 
       <View>
-
-        <View style={styles.memoListItem}>
-          <View>  
-            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
-            <Text style={styles.memoListItemDate}>2025年3月27日 10:00</Text>
-          </View>
-          <View>
-            <Text>X</Text>
-          </View>
-        </View>
-
-        
-        <View style={styles.memoListItem}>
-          <View>  
-            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
-            <Text style={styles.memoListItemDate}>2025年3月27日 10:00</Text>
-          </View>
-          <View>
-            <Text>X</Text>
-          </View>
-        </View>
-
-
-        <View style={styles.memoListItem}>
-          <View>  
-            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
-            <Text style={styles.memoListItemDate}>2025年3月27日 10:00</Text>
-          </View>
-          <View>
-            <Text>X</Text>
-          </View>
-        </View>
+        <MemoListItem />
+        <MemoListItem />
+        <MemoListItem />
         
       </View>
 
-      <View style={styles.circleButton}>
-        <Text style={styles.circleButtonLabel}>+</Text>
-      </View>
+      <CircleButton/>
+      
     </View>
   )
   
@@ -58,66 +28,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor:"#ffffff"
-  },
-  header:{
-    backgroundColor:"#467FD3",
-    height: 84,
-    justifyContent: "flex-end"
-  },
-  headerInner:{
-    alignItems:"center"
-  },
-  headerRight:{
-    position:"absolute",
-    right: 16,
-    bottom: 16,
-    color:"rgba(255,255,255,0.7)"
-  },
-  headerTitle:{
-    marginBottom: 8,
-    fontSize: 22,
-    lineHeight: 32,
-    fontWeight:"bold",
-    color: "#ffffff"
-  },
-  memoListItem:{
-    backgroundColor: "#ffffff",
-    flexDirection: "row",
-    justifyContent:"space-between",
-    paddingVertical: 16,
-    paddingHorizontal: 19,
-    alignItems: "center",
-    borderBottomWidth: 1,
-    borderColor: "rgba(0,0,0,0.15)"
-  },
-  memoListItemTitle:{
-    fontSize: 16,
-    lineHeight: 32
-  },
-  memoListItemDate:{
-    fontSize: 12,
-    lineHeight: 16
-  },
-  circleButton:{
-    width: 64,
-    height: 64,
-    borderRadius: 32,
-    backgroundColor:"#467fd3",
-    justifyContent: "center",
-    alignItems: "center",
-    position: "absolute",
-    right: 40,
-    bottom: 40,
-    shadowColor: "#000000",
-    shadowOpacity: 0.25,
-    shadowRadius: 8,
-    shadowOffset: {width: 0, height: 8},
-    elevation: 8
-  },
-  circleButtonLabel:{
-    color: "#ffffff",
-    fontSize: 40,
-    lineHeight: 38
   }
 })
 
---
+++ src/components/CircleButton.tsx
@@ -0,0 +1,36 @@
+import { View, Text, StyleSheet } from "react-native"
+
+const CircleButton = () => {
+  return(
+    <View style={styles.circleButton}>
+        <Text style={styles.circleButtonLabel}>+</Text>
+      </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  
+  circleButton:{
+    width: 64,
+    height: 64,
+    borderRadius: 32,
+    backgroundColor:"#467fd3",
+    justifyContent: "center",
+    alignItems: "center",
+    position: "absolute",
+    right: 40,
+    bottom: 40,
+    shadowColor: "#000000",
+    shadowOpacity: 0.25,
+    shadowRadius: 8,
+    shadowOffset: {width: 0, height: 8},
+    elevation: 8
+  },
+  circleButtonLabel:{
+    color: "#ffffff",
+    fontSize: 40,
+    lineHeight: 38
+  }
+})
+
+export default CircleButton

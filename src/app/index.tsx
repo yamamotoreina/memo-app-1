cbc16a7c8af64dec0907d6c80e4caf0c589ce8cf
yamamotoreina <rainbowsunny953@gmail.com>
2025-07-28 18:25:39 +0900

    View Finish making

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,7 +1,7 @@
 import { Redirect } from "expo-router"
 
 const Index = (): JSX.Element => {
-  return <Redirect href='memo/Edit'/>
+  return <Redirect href='auth/sign_up'/>
 }
 
 export default Index

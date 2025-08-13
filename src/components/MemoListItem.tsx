95b3895ec5e36537d46a74b13c48b5652879d117
yamamotoreina <rainbowsunny953@gmail.com>
2025-08-13 19:54:04 +0900

    削除ボタンのTouchableOpacityのonPressを実装

--- src/components/MemoListItem.tsx
+++ src/components/MemoListItem.tsx
@@ -1,27 +1,58 @@
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native"
-import { Link } from 'expo-router'
+import { View, Text, StyleSheet, TouchableOpacity, Alert } from "react-native"
+import { Link } from "expo-router"
+import { deleteDoc, doc } from "firebase/firestore"
 
-import Icon from './Icon'
-import { type Memo } from "../../types/memo" 
+import Icon from "./Icon"
+import { type Memo } from "../../types/memo"
+import { auth, db } from "../config"
 
 interface Props {
   memo: Memo
 }
 
+const handlePress = (id: string): void => {
+  if (auth.currentUser === null) {
+    return
+  }
+  const ref = doc(db, `users/${auth.currentUser.uid}/memos`, id)
+  Alert.alert("メモを削除します", "宜しいですか？", [
+    {
+      text: "キャンセル"
+    },
+    {
+      text: "削除する",
+      style: "destructive",
+      onPress: () => {
+        deleteDoc(ref).catch(() => {
+          Alert.alert("削除に失敗しました")
+        })
+      }
+    }
+  ])
+}
+
 const MemoListItem = (props: Props): JSX.Element | null => {
   const { memo } = props
-  const { bodyText, updatedAt} = memo
-  if ( bodyText === null || updatedAt === null ) { return null }
-  const dateString =updatedAt.toDate().toLocaleString('ja-JP')
+  const { bodyText, updatedAt } = memo
+  if (bodyText === null || updatedAt === null) {
+    return null
+  }
+  const dateString = updatedAt.toDate().toLocaleString("ja-JP")
   return (
-    <Link href={{ pathname : '/memo/Detail', params: { id: memo.id }}} asChild>
+    <Link href={{ pathname: "/memo/Detail", params: { id: memo.id } }} asChild>
       <TouchableOpacity style={styles.memoListItem}>
         <View>
-          <Text numberOfLines={1} style={styles.memoListItemTitle}>{bodyText}</Text>
+          <Text numberOfLines={1} style={styles.memoListItemTitle}>
+            {bodyText}
+          </Text>
           <Text style={styles.memoListItemDate}>{dateString}</Text>
         </View>
-        <TouchableOpacity>
-          <Icon name='delete' size={32} color="#B0B0B0" />
+        <TouchableOpacity
+          onPress={() => {
+            handlePress(memo.id)
+          }}
+        >
+          <Icon name="delete" size={32} color="#B0B0B0" />
         </TouchableOpacity>
       </TouchableOpacity>
     </Link>
@@ -29,22 +60,21 @@ const MemoListItem = (props: Props): JSX.Element | null => {
 }
 
 const styles = StyleSheet.create({
-  
-  memoListItem:{
+  memoListItem: {
     backgroundColor: "#ffffff",
     flexDirection: "row",
-    justifyContent:"space-between",
+    justifyContent: "space-between",
     paddingVertical: 16,
     paddingHorizontal: 19,
     alignItems: "center",
     borderBottomWidth: 1,
     borderColor: "rgba(0,0,0,0.15)"
   },
-  memoListItemTitle:{
+  memoListItemTitle: {
     fontSize: 16,
     lineHeight: 32
   },
-  memoListItemDate:{
+  memoListItemDate: {
     fontSize: 12,
     lineHeight: 16
   }
